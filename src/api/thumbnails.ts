fcf16972098f2c635678c326a52717bb65fda130
SpottedHorse <alexbelanger1996@hotmail.com>
2026-04-08 07:48:57 -0400

    refactor with solution files

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -5,40 +5,10 @@ import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 import path from "path";
+import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
 // import { getInMemoryURL } from "./assets";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
 const MAX_UPLOAD_SIZE = 10 << 20
-
-// const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-// export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-//   const { videoId } = req.params as { videoId?: string };
-//   if (!videoId) {
-//     throw new BadRequestError("Invalid video ID");
-//   }
-
-//   const video = getVideo(cfg.db, videoId);
-//   if (!video) {
-//     throw new NotFoundError("Couldn't find video");
-//   }
-
-//   const thumbnail = videoThumbnails.get(videoId);
-//   if (!thumbnail) {
-//     throw new NotFoundError("Thumbnail not found");
-//   }
-
-//   return new Response(video.thumbnailURL, {
-//     headers: {
-//       "Content-Type": thumbnail.mediaType,
-//       "Cache-Control": "no-store",
-//     },
-//   });
-// }
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -62,45 +32,32 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   const formData = await req.formData();
 
-  const data = formData.get('thumbnail');
+  const file = formData.get('thumbnail');
 
-  if (!(data instanceof File)) {
+  if (!(file instanceof File)) {
     throw new BadRequestError('Thumbnail file missing');
   }
 
-  if (data.size > MAX_UPLOAD_SIZE) {
+  if (file.size > MAX_UPLOAD_SIZE) {
     throw new BadRequestError(
       `Thumbnail file exceeds the maximum allowed size of 10MB`
     );
   }
 
-  const mediaType = data.type;
-  if (!mediaType) {
-    throw new BadRequestError("Missing Content-Type for thumbnail");
+  const mediaType = file.type;
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Invalid file type. Only JPEG or PNG allowed.");
   }
 
-  const fileData = await data.arrayBuffer();
-  if (!fileData) {
-    throw new Error("Error reading file data");
-  }
+  const ext = mediaTypeToExt(mediaType);
+  const filename = `${videoId}${ext}`;
 
-  // videoThumbnails.set(videoId, {
-  //   data: fileData, 
-  //   mediaType: mediaType
-  // });
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, file);
 
-  // const buffer = Buffer.from(fileData);
-  console.log(`mediaType: ${mediaType}`)
-  const filePath = path.join(cfg.assetsRoot, `${videoId}.${mediaType.split('/')[1]}`)
-  console.log(`filePath: ${filePath}`)
-  await Bun.write(`${filePath}`, fileData)
-  // const bufferString = buffer.toString("base64");
-  // const dataUrl = `data:${mediaType};base64,${bufferString}`;
+  const urlPath = getAssetURL(cfg, filename);
+  video.thumbnailURL = urlPath;
 
-  const thumbnail_url = path.join(`http://localhost:${cfg.port}`, filePath)
-
-  video.thumbnailURL = thumbnail_url;
-  // video.thumbnailURL = getInMemoryURL(cfg, videoId);
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
